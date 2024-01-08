3d9314b901023c837c869e37244e969482822867
Renan Sies <renansies@gmail.com>
2024-01-08 20:08:40 -0300

    Enemy and Damage Behaviour

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -83,6 +83,7 @@ public class PlayerController : MonoBehaviour
         {
             Instance = this;
         }
+        health = maxHealth;
     }
     // Start is called before the first frame update
     void Start()
